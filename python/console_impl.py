ef4ec14ccb049c2812594a21293bfc49e1b012d6
duxiuxing <duxiuxing@foxmail.com>
2024-09-06 07:28:16 +0800

    rename rom files: D

--- python/console_impl.py
+++ python/console_impl.py
@@ -4,25 +4,12 @@ import fnmatch
 import os
 import shutil
 import xml.etree.ElementTree as ET
-import zlib
 
 from console import Console
 from game_info import GameInfo
+from helper import Helper
 from local_configs import LocalConfigs
 from wiiflow import WiiFlow
-
-
-def compute_crc32(file_path):
-    # 计算指定文件的 CRC32 值
-    # Args:
-    #     file_path (str): 文件路径，通常是游戏的 ROM 文件
-    # Returns:
-    #     str: 文件的 CRC32 值，八位大写十六进制字符串
-    with open(file_path, 'rb') as file:
-        data = file.read()
-        crc = zlib.crc32(data)
-        crc32 = hex(crc & 0xFFFFFFFF)[2:].upper()
-        return crc32.rjust(8, "0")
 
 
 class ConsoleImpl(Console):
@@ -57,6 +44,8 @@ class ConsoleImpl(Console):
             root = tree.getroot()
             for game_elem in root.findall("Game"):
                 rom_crc32 = game_elem.get("crc32").rjust(8, "0")
+                rom_title = game_elem.get("rom")
+                en_title = game_elem.get("en")
                 zhcn_title = game_elem.get("zhcn")
                 rom_path = os.path.join(letter_folder_path, f"{zhcn_title}{self.rom_extension()}")
                 if not os.path.exists(rom_path):
@@ -67,9 +56,20 @@ class ConsoleImpl(Console):
 
                 self.roms_crc32_to_path[rom_crc32] = rom_path
 
+                db_game_info = self.wiiflow().plugins_data().query_game_info(rom_crc32=rom_crc32,
+                                                                             rom_title=rom_title,
+                                                                             en_title=en_title,
+                                                                             zhcn_title=zhcn_title)
+                if db_game_info is not None:
+                    if rom_title is None:
+                        rom_title = db_game_info.rom_title
+                        en_title = db_game_info.en_title
+
                 game_info = GameInfo(
                     rom_crc32=rom_crc32,
                     rom_bytes=game_elem.get("bytes"),
+                    rom_title=rom_title,
+                    en_title=en_title,
                     zhcn_title=zhcn_title)
                 self.roms_crc32_to_game_info[rom_crc32] = game_info
 
@@ -82,33 +82,25 @@ class ConsoleImpl(Console):
         else:
             return None
 
-    def verify_rom_name_as_crc32(self, rom_name):
-        # 以《1941》这个游戏的 ROM 文件（1941.zip）为例：
-        # 情况1. 当游戏和 ROM 文件一一对应时，文件路径是：cps-player\\cps1\\roms\\1941.zip
-        # 情况2. 当游戏对应的 ROM 文件不止一个时，需要先创建一个 1941 的文件夹，然后把
+    def verify_rom_name_as_crc32(self, old_rom_path):
+        # 以《大金刚》这个游戏的 ROM 文件（Donkey Kong.nes）为例：
+        # 情况1. 当游戏和 ROM 文件一一对应时，文件路径是：nes-player\\roms\\D\\Donkey Kong.nes
+        # 情况2. 当游戏对应的 ROM 文件不止一个时，需要先创建一个 Donkey Kong 的文件夹，然后把
         #        不同的 ROM 文件以 CRC32 值命名，放到这个文件夹里，例如：
-        #          - roms\\1941\\64E58DC3.zip
-        #          - roms\\1941\\8C733532.zip
-        #          - roms\\1941\\9DA9C6D9.zip
+        #          - roms\\D\\Donkey Kong\\73FF4BA1.nes
+        #          - roms\\D\\Donkey Kong\\E40B593B.nes
         #
         # 本函数仅在 self.import_roms() 中调用，用来把情况1的 ROM 文件按照情况2的规则重命名
-        default_rom_path = os.path.join(
-            self.root_folder_path(), f"roms\\{rom_name}")
-        if not os.path.exists(default_rom_path):
-            return
-
+        rom_crc32 = Helper.compute_crc32(old_rom_path)
+        rom_name = os.path.basename(old_rom_path)
         rom_title = os.path.splitext(rom_name)[0]
         rom_extension = os.path.splitext(rom_name)[1]
 
-        rom_folder_path = os.path.join(
-            self.root_folder_path(), f"roms\\{rom_title}")
-        if not os.path.exists(rom_folder_path):
-            os.makedirs(rom_folder_path)
-
-        dst_rom_path = os.path.join(
-            rom_folder_path,
-            f"{compute_crc32(default_rom_path)}{rom_extension}")
-        os.rename(default_rom_path, dst_rom_path)
+        new_rom_path = os.path.join(os.path.dirname(old_rom_path),
+                                    f"{rom_title}\\{rom_crc32}{rom_extension}")
+        if Helper.verify_folder_exist_ex(os.path.dirname(new_rom_path)):
+            os.rename(old_rom_path, new_rom_path)
+            self.roms_crc32_to_path[rom_crc32] = new_rom_path
 
     def import_roms(self):
         # 本函数用于导入 roms_import 文件夹里的 ROM 文件
@@ -132,7 +124,7 @@ class ConsoleImpl(Console):
                 continue
 
             src_rom_path = os.path.join(import_folder_path, src_rom_name)
-            src_rom_crc32 = compute_crc32(src_rom_path)
+            src_rom_crc32 = Helper.compute_crc32(src_rom_path)
             if src_rom_crc32 in self.roms_crc32_to_game_info.keys():
                 exist_roms_crc32_to_name[src_rom_crc32] = src_rom_name
                 continue
@@ -146,7 +138,7 @@ class ConsoleImpl(Console):
                                                                       en_title=src_rom_title,
                                                                       zhcn_title=src_rom_title)
             if game_info is None:
-                print(f"未知的新游戏 {src_rom_name}，crc32 = {src_rom_crc32}")
+                print(f"未知的游戏 crc32=\"{src_rom_crc32}\" bytes=\"{src_rom_bytes}\"\n\trom=\"{src_rom_title}\"")
                 continue
 
             en_title = game_info.en_title
@@ -155,19 +147,47 @@ class ConsoleImpl(Console):
             attribs = {
                 "crc32": src_rom_crc32,
                 "bytes": src_rom_bytes,
-                "rom": src_rom_name,
+                "rom": game_info.rom_title,
                 "en": en_title,
                 "zhcn": zhcn_title
             }
+            if src_rom_title[-3:] == "(中)":
+                attribs["language"] = "chinese"
             ET.SubElement(new_roms_xml_root, "Game", attribs)
+            self.roms_crc32_to_game_info[src_rom_crc32] = GameInfo(rom_crc32=src_rom_crc32,
+                                                                   rom_bytes=src_rom_bytes,
+                                                                   rom_title=game_info.rom_title,
+                                                                   en_title=en_title,
+                                                                   zhcn_title=zhcn_title)
 
-            self.roms_crc32_to_game_info[src_rom_crc32] = GameInfo(
-                rom_crc32=src_rom_crc32,
-                rom_bytes=src_rom_bytes,
-                en_title=en_title,
-                zhcn_title=zhcn_title)
+            if src_rom_title == game_info.rom_title:
+                print(f"新游戏入库 {src_rom_name}，crc32 = {src_rom_crc32}")
+            else:
+                print(f"新游戏 {src_rom_name} 官方名称为 {game_info.rom_title}，crc32 = {src_rom_crc32}")
 
-            print(f"新游戏 {src_rom_name}，crc32={src_rom_crc32}，en = {en_title}，zhcn = {zhcn_title}")
+            letter_folder_name = game_info.rom_title.upper()[0]
+            if letter_folder_name not in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+                letter_folder_name = "#"
+
+            dst_rom_path = os.path.join(self.root_folder_path(),
+                                        f"roms\\{letter_folder_name}\\{game_info.rom_title}{src_rom_extension}")
+            if os.path.exists(dst_rom_path):
+                # 如果已有的 ROM 文件是按照情况1的规则命名的，需要将已有的 ROM 文件按照情况2的规则重命名
+                self.verify_rom_name_as_crc32(dst_rom_path)
+                dst_rom_path = os.path.join(self.root_folder_path(),
+                                            f"roms\\{letter_folder_name}\\{game_info.rom_title}\\{src_rom_crc32}{src_rom_extension}")
+            else:
+                # 如果已有的 ROM 文件是按照情况2的规则命名的，那新的 ROM 文件也按照情况2的规则重命名
+                rom_folder_path = os.path.join(self.root_folder_path(),
+                                               f"roms\\{letter_folder_name}\\{game_info.rom_title}")
+                if os.path.exists(rom_folder_path):
+                    dst_rom_path = os.path.join(self.root_folder_path(),
+                                                f"roms\\{letter_folder_name}\\{game_info.rom_title}\\{src_rom_crc32}{src_rom_extension}")
+
+            if os.path.exists(dst_rom_path):
+                print(f"{src_rom_name}，crc32 = {src_rom_crc32} 已经存在，但不在 roms.xml 中")
+            elif Helper.verify_folder_exist_ex(os.path.dirname(dst_rom_path)):
+                os.rename(src_rom_path, dst_rom_path)
             new_roms_count = new_roms_count + 1
 
         xml_file_path = os.path.join(self.root_folder_path(), "roms_exist.xml")
@@ -185,7 +205,7 @@ class ConsoleImpl(Console):
                     "zhcn": game_info.zhcn_title
                 }
                 ET.SubElement(exist_roms_xml_root, "Game", attribs)
-                print(f"{rom_name} 已经存在，{game_info.rom_name} crc32 = {rom_crc32}")
+                print(f"{rom_name} 已经存在，{game_info.rom_title} crc32 = {rom_crc32}")
             ET.ElementTree(exist_roms_xml_root).write(xml_file_path, encoding="utf-8", xml_declaration=True)
 
         xml_file_path = os.path.join(self.root_folder_path(), "roms_new.xml")
